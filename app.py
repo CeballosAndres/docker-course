c0bdbec488d714f741ac7cbabc5df081ef770b35
José Andrés Ceballos Vadillo <andres@ceballos.dev>
2022-06-22 02:34:13 -0500

    Change mysql version

--- app.py
+++ app.py
@@ -1,20 +1,26 @@
 from flask import Flask
-import pymysql
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
-connection = pymysql.connect(host="db", user="root", password="root",
-                             db="course", port=3307, charset='utf8mb4', cursorclass=pymysql.cursors.DictCursor)
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_PORT'] = 3307
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'root'
+app.config['MYSQL_DB'] = 'course'
+ 
+mysql = MySQL(app)
+
 
 
 @app.route('/')
 def hello_world():
-    # Creating a connection cursor
-    cursor = connection.cursor()
-    # Executing SQL Statements
+    #Creating a connection cursor
+    cursor = mysql.connection.cursor()
+    #Executing SQL Statements
     cursor.execute(''' SELECT * FROM course_users ''')
     course_users = cursor.fetchall()
-    # Closing the cursor
+    #Closing the cursor
     cursor.close()
     print(str(course_users))
     return str(course_users)
